9fdfcd1b9bd005eb1aab12a7f3b12df1950f7b47
Premsai Ellendula <154212425+Premsai03@users.noreply.github.com>
2024-10-29 10:47:50 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
       <form action="https://api.web3forms.com/submit" method="POST">
         <h1 className='text-black font-wixMadeforDisplay pt-6 text-8xl lg:pt-16'>contact</h1>
         <input type="hidden" name="access_key" value="5838d4bb-7abf-4380-b418-84ea57dff1a6"></input>
-        <input type="hidden" name="redirect" value="https:/premsaiel.netlify.app/thankyou"></input>
+        <input type="hidden" name="redirect" value="https://premsaiel.netlify.app/thankyou"></input>
         <input type="text" name="name" placeholder='name' required className='w-full font-wixMadeforText text-xl outline-none bg-transparent text-gray-500 lg:rounded-3xl rounded-full border border-black mt-10 p-4' />
         <input type="email" name="email" placeholder='email@example.com' required className='w-full font-wixMadeforText text-xl outline-none bg-transparent text-gray-500 lg:rounded-3xl rounded-full border border-black mt-10 p-4' />
         <textarea required rows={2} className='w-full font-wixMadeforText text-xl outline-none bg-transparent text-gray-500 rounded-3xl border border-black mt-10 p-4' name="message" placeholder='message'></textarea>
