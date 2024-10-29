028bd69450f080dc336e41130aa34c70830c206f
Premsai Ellendula <154212425+Premsai03@users.noreply.github.com>
2024-10-29 14:04:28 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,13 +1,13 @@
 
 export const PROJECTS = [
     {
-        url: "https://github.com/Premsai03/Amazon-clone",
+        url: "premsai03.github.io/E-commerce-clone/",
     },
     {
         url: "https://github.com/Premsai03/Password-Generator",
     },
     {
-        url: "https://github.com/Premsai03/Portfolio",
+        url: "https://premsaiel.netlify.app/",
     },
     {
         url: "https://github.com/Premsai03/Tesla-UI",
